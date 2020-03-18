1fa9fda31857d956b161b3b3f3eccda173744282
maxima-us <maximousse@protonmail.com>
2020-03-18 17:09:56 +0400

    test for html views

---
+++ tests/server/views/html/test_private_html.py
@@ -0,0 +1,51 @@
+import logging
+
+import httpx
+from pydantic import ValidationError
+from starlette.responses import HTMLResponse
+from starlette.testclient import TestClient
+
+from server.main_app import app
+from server import settings
+
+api = TestClient(app)
+
+session = httpx.AsyncClient()
+settings.SESSION = session
+
+
+
+def test_get_account_balance():
+    response = api.get("/html/private/account_balance/kraken")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_trade_balance():
+    response = api.get("/html/private/trade_balance/kraken")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_open_orders():
+    response = api.get("/html/private/open_orders/kraken")
+    assert response.status_code == 200 
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_closed_orders():
+    response = api.get("/html/private/closed_orders/kraken")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_user_trades():
+    response = api.get("/html/private/trades/kraken")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_open_positions():
+    response = api.get("/html/private/open_positions/kraken")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
---
+++ tests/server/views/html/test_public_html.py
@@ -0,0 +1,45 @@
+import logging
+
+import httpx
+from pydantic import ValidationError
+from starlette.responses import HTMLResponse
+from starlette.testclient import TestClient
+
+from server.main_app import app
+from server import settings
+
+api = TestClient(app)
+
+session = httpx.AsyncClient()
+settings.SESSION = session
+
+
+
+def test_get_ticker():
+    response = api.get("/html/public/ticker/kraken?pair=xbt-usd")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_ohlc():
+    response = api.get("/html/public/ohlc/kraken?pair=xbt-usd&timeframe=240")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_orderbook():
+    response = api.get("/html/public/orderbook/kraken?pair=xbt-usd")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_trades():
+    response = api.get("/html/public/trades/kraken?pair=xbt-usd")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+def test_get_spread():
+    response = api.get("/html/public/spread/kraken?pair=xbt-usd")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
