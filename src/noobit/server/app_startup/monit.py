f642fd97b394b531bae8da483d73f3b2cd9b2901
maxima-us <maximousse@protonmail.com>
2020-06-03 18:10:24 +0400

    add monitrc template

--- src/noobit/server/app_startup/monit.py
+++ src/noobit/server/app_startup/monit.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import logging
 import shutil
 
 
@@ -7,8 +8,9 @@ def startup_monit():
 
     # Check if we have a file at path: /etc/monit/monitrc
     if not os.path.isfile("/etc/monit/monitrc"):
-        # shutil.copy("monitrc", "/etc/monit/monitrc")
-        raise OSError("File missing at path: /etc/monit/monitrc. Please refer to the following guide: https://guides.wp-bullet.com/configure-monit-send-email-alerts-mailgun/")
+        logging.warning("File missing at path: /etc/monit/monitrc. Copying template to path")
+        shutil.copy("monitrc", "/etc/monit/monitrc")
+        logging.warning("Please refer to the following guide for configuration: https://guides.wp-bullet.com/configure-monit-send-email-alerts-mailgun/")
 
     # Validate syntax from monit control file ==> check again later, this doesnt seem to work
     # as check_output raises an error whenever stdout returns sthg (not empty)
