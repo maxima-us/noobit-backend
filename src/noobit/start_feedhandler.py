4a97175da86c0557ae924e135045a48f04d3f1a4
maxima-us <maximousse@protonmail.com>
2020-06-28 18:45:19 +0400

    cleanup

--- src/noobit/start_feedhandler.py
+++
@@ -1,5 +0,0 @@
-from processor.feed_handler import FeedHandler
-
-test = FeedHandler(exchanges=["kraken"], private_feeds=["ownTrades", "openOrders"], public_feeds=["trade", "spread"], pairs=["XBT/USD"])
-
-test.run()
--- src/noobit/start_server.py
+++
@@ -1,7 +0,0 @@
-from server import main_server
-
-def run_server():
-
-    main_server.run("server.main_app:app", host='localhost', port=8000, reload=False)
-
-run_server()
